6a07eb651f436dff687d390c5eda050cf0ea48ca
Jiang Le <smartynaoki@gmail.com>
2022-04-26 12:12:15 +0800

    update repo

--- hpspp_centos7.sh
+++ hpspp_centos7.sh
@@ -6,7 +6,7 @@ rpm --import https://downloads.linux.hpe.com/SDR/hpePublicKey2048_key1.pub
 cat > /etc/yum.repos.d/spp.repo << SPPREPO
 [spp]
 name=Service Pack for ProLiant
-baseurl=http://downloads.linux.hpe.com/repo/spp-gen8/redhat/7/x86_64/current
+baseurl=https://downloads.linux.hpe.com/SDR/repo/spp/RHEL/7.9/x86_64/current/
 enabled=1
 gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/GPG-KEY-ServicePackforProLiant
